767e5d786ff138f387c2d40a3fa27fd2f2bc28dd
melvizah <melvinazahras.com>
2026-05-02 10:54:01 +0700

    edit siswa jadwal

--- siswa/jadwal.js
+++ siswa/jadwal.js
@@ -86,9 +86,9 @@ function renderProfil(siswa) {
 
   const semesterSelect = document.getElementById("semesterSelect");
   if (semesterSelect) {
-    semesterSelect.innerHTML =
-      `<option>${siswa.tahun_ajaran || "-"} - ${siswa.semester || "-"}</option>`;
-  }
+    semesterSelect.innerHTML = result.semester_list.map(item =>
+  `<option value="${item.id}">${item.tahun} - ${item.semester}</option>`
+).join("");
 
   document.getElementById("judulTabel").textContent =
     `Jadwal Pelajaran Kelas ${siswa.kelas} (Minggu Ini)`;
