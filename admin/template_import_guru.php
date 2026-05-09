07b180569eeded907ab978b88948981ea163a0a6
Nazlasalsabila <nzla78950@gmail.com>
2026-05-09 11:22:03 +0700

    bismillah

--- admin/template_import_guru.php
+++ admin/template_import_guru.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'koneksi.php';
+
 $filename = "template_import_guru.csv";
 
 header("Content-Type: text/csv; charset=utf-8");
@@ -21,8 +23,24 @@ fputcsv($output, [
     "Contoh Nama Guru",
     "contoh.guru@email.com",
     "L",
-    "Matematika"
+    "IPS"
 ], ";");
+
+fputcsv($output, [], ";");
+fputcsv($output, ["# Panduan pengisian"], ";");
+fputcsv($output, ["# jenis_kelamin isi L untuk Laki-laki atau P untuk Perempuan"], ";");
+fputcsv($output, ["# Kolom mapel harus sama dengan salah satu nama mapel di bawah ini"], ";");
+fputcsv($output, ["# Daftar mapel valid:"], ";");
+
+if (isset($conn) && !$conn->connect_error) {
+    $query = mysqli_query($conn, "SELECT nama_mapel FROM mapel ORDER BY nama_mapel ASC");
+
+    if ($query) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            fputcsv($output, ["# " . $row['nama_mapel']], ";");
+        }
+    }
+}
 
 fclose($output);
 exit;
